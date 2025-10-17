8670e6230b9d7ba3e1eb4fdaef94304b9ce1ef2a
JulitKuchta <julita.kuchta@student.pk.edu.pl>
2025-10-17 21:56:36 +0200

    zad 4.3.

--- models/product.js
+++ models/product.js
@@ -1,15 +1,50 @@
 const products = [];
+const fs = require("fs").promises;
+const path = require("path");
+
+const fullName = path.join(
+  path.dirname(require.main.filename),
+  "data",
+  "products.json"
+);
+
+const getProductsFromFile = async () => {
+  try {
+    const contents = await fs.readFile(fullName, "utf8");
+    if (contents.length === 0) {
+      return [];
+    } else {
+      return JSON.parse(contents);
+    }
+  } catch (error) {
+    //console.log(error);
+    return [];
+  }
+};
 
 module.exports = class Product {
   constructor(t) {
     this.title = t;
-  }
-  save() {
     this.price = Math.floor(Math.random() * 90) + 10;
-    this.desc = "A very nice " + this.title;
-    products.push(this);
+    this.desc = "A very nice" + this.title;
   }
-  static fetchAll() {
+
+  async save() {
+    products.length = 0;
+    try {
+      const prods = await getProductsFromFile();
+      products.push(...prods);
+      products.push(this);
+      await fs.writeFile(fullName, JSON.stringify(products));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async fetchAll() {
+    products.length = 0;
+    const prods = await getProductsFromFile();
+    products.push(...prods);
     return products;
   }
 };
